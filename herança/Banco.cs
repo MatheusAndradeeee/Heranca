752d163fca6027ff18253664ccdd14c0ab9685fa
Matheus Andrade <mateusandracosta@gmail.com>
2023-08-19 16:47:02 -0400

    primeiro teste

---
+++ herança/Banco.cs
@@ -0,0 +1,25 @@
+﻿/*No banco XX, os clientes podem criar contas bancárias. Cada conta possui os
+atributos: número da conta, agência, titular da conta (apenas o nome), saldo da conta.
+Além disso, as contas possuem as operações de saque (que verifica se há saldo
+suficiente) e depósito. O banco possui dois tipos de contas diferenciadas, conta para
+estudante e conta para empresa*/public class Banco
+{
+    private string numConta;
+    private string agencia;
+    private string nome;
+    private double saldo;
+
+    public void GetDeposito(double _deposito)
+    {
+        this.saldo = _deposito;
+    }
+    public double SetSaque(double _saque)
+    {
+        if(_saque <= this.saldo)
+        {
+            _saque -= this.saldo;
+        }
+    }
+
+
+}
